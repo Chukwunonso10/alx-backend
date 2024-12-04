c5d41daa91fc6d495fba793c9a36391491310904
Chukwunonos10 <kuzuechinonsojude@gmail.com>
2024-12-04 12:29:55 -0800

    solutions

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -1,34 +1,27 @@
-import { createClient, print } from "redis";
-import { promisify } from "util";
+import redis from 'redis';
+import { promisify } from 'util';
 
-const client = createClient();
+const client = redis.createClient();
+const getAsync = promisify(client.get).bind(client);
 
-client
-  .on("connect", () => {
-    console.log("Redis client connected to the server");
-  })
-  .on("error", (error) => {
-    console.log(`Redis client not connected to the server: ${error}`);
-  });
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server: ', err.message);
+});
 
 function setNewSchool(schoolName, value) {
-  client.set(schoolName, value, print);
+  client.set(schoolName, value, redis.print);
 }
 
+function displaySchoolValue(schoolName) {
+  getAsync(schoolName).then((reply) => {
+    console.log(reply);
+  });
+}
 
-
-const get = promisify(client.get).bind(client);
-
-async function displaySchoolValue(schoolName) {
-    const res = await client.get(schoolName).catch((error) => {
-      if (error) {
-        console.log(error);
-        throw error;
-      }
-    });
-    console.log(res);
-  }
-
-displaySchoolValue("Holberton");
-setNewSchool("HolbertonSanFrancisco", "100");
-displaySchoolValue("HolbertonSanFrancisco");
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
--- 0x03-queuing_system_in_js/5-subscriber.js
+++ 0x03-queuing_system_in_js/5-subscriber.js
@@ -9,3 +9,13 @@ client.on('connect', () => {
 client.on('error', (err) => {
   console.log('Redis client not connected to the server: ', err.message);
 });
+
+client.subscribe('holberton school channel');
+
+client.on('message', (channel, message) => {
+  console.log(message);
+  if (message === 'KILL_SERVER') {
+    client.unsubscribe('holberton school channel');
+    client.quit();
+  }
+});
